97c23c12671bb20d40de8069f99a13219a26fe2a
Jordan Masone <jordanmasone1@gmail.com>
2021-01-27 18:32:18 -0500

    Added starting inquirer function.

--- db/index.js
+++ db/index.js
@@ -12,5 +12,30 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log(`Connected as id ${connection.threadId} \n`);
-    connection.end();
+    start();
 });
+
+function start() {
+    inquirer.prompt([
+        {
+            name: "userChoice",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View all employees", "View all roles", "View all departments", "EXIT"]
+        }
+    ]).then(function (answer) {
+        if (answer.userChoice === "View all employees") {
+            viewEmployees();
+        } 
+        else if (answer.userChoice === "View all roles") {
+            viewRoles();
+        }
+        else if (answer.userChoice === "View all departments") {
+            viewDepartments();
+        } else {
+            console.log("Shutting down...");
+            connection.end();
+        }
+    });
+}
+
