39fee3fa986d0acfdfd9e9c59d6def1bc9458371
Jordan Masone <jordanmasone1@gmail.com>
2021-01-28 00:41:40 -0500

    Added figlet and chalk npm packages.

--- db/index.js
+++ db/index.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const figlet = require("figlet");
+const chalk = require("chalk");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -12,6 +14,11 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log(`Connected as id ${connection.threadId} \n`);
+    console.log(
+        chalk.magenta(
+            figlet.textSync("Employee Tracker", {horizonalLayout: "full"})
+        )
+    );
     start();
 });
 
@@ -21,17 +28,38 @@ function start() {
             name: "userChoice",
             type: "list",
             message: "What would you like to do?",
-            choices: ["View all employees", "View all roles", "View all departments", "EXIT"]
+            choices: [
+                "View all employees",
+                "View all roles",
+                "View all departments",
+                "Update employee roles",
+                "Add new department",
+                "Add new role",
+                "Add new employee",
+                "EXIT"
+            ]
         }
     ]).then(function (answer) {
         if (answer.userChoice === "View all employees") {
             viewTable("employee");
-        } 
+        }
         else if (answer.userChoice === "View all roles") {
             viewTable("role");
         }
         else if (answer.userChoice === "View all departments") {
             viewTable("department");
+        }
+        else if (answer.userChoice === "Update employee roles") {
+            updateRole();
+        }
+        else if (answer.userChoice === "Add new department") {
+            addDepartment();
+        }
+        else if (answer.userChoice === "Add new role") {
+            addRole();
+        }
+        else if (answer.userChoice === "Add new employee") {
+            addEmployee();
         } else {
             console.log("Shutting down...");
             connection.end();
@@ -45,5 +73,159 @@ function viewTable(value) {
         if (err) throw err;
         console.table(res);
         start();
+    });
+}
+
+function updateRole() {
+
+}
+
+function addDepartment() {
+    console.log("Please enter the new department info...");
+    inquirer.prompt([
+        {
+            name: "departName",
+            type: "input",
+            message: "What is the name of the new department?"
+        }
+    ]).then(function (answer) {
+        connection.query(
+            "INSERT INTO department SET ?",
+            {
+                name: answer.departName
+            },
+            function (err, res) {
+                if (err) throw err;
+                console.log(`${answer.departName} department added!`);
+                inquirer.prompt([
+                    {
+                        name: "another",
+                        type: "confirm",
+                        message: "Would you like to make another department?"
+                    }
+                ]).then(function (answer) {
+                    if (answer.another === true) {
+                        addDepartment();
+                    } else {
+                        start();
+                    }
+                });
+            }
+        );
+    });
+}
+
+function addRole() {
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+
+        console.log("Please enter the new role info...");
+        inquirer.prompt([
+            {
+                name: "department",
+                type: "list",
+                message: "What department is this new role in?",
+                choices: function () {
+                    let choicesArray = [];
+                    for (let i = 0; i < res.length; i++) {
+                        choicesArray.push(res[i].name);
+                    }
+                    return choicesArray;
+                }
+            },
+            {
+                name: "roleTitle",
+                type: "input",
+                message: "What is the title of the new role?"
+            },
+            {
+                name: "roleSalary",
+                type: "number",
+                message: "What is the salary of the new role?"
+            },
+        ]).then(function (answer) {
+            connection.query(
+                "INSERT INTO role SET ?",
+                {
+                    title: answer.roleTitle,
+                    salary: answer.roleSalary,
+                    department_id: function () {
+                        if (answer.department === "Sales") {
+                            return 1;
+                        }
+                        else if (answer.department === "Accounting") {
+                            return 2;
+                        }
+                        else if (answer.department === "Engineering") {
+                            return 3;
+                        }
+                        else if (answer.department === "Human Resources") {
+                            return 4;
+                        }
+                        else if (answer.department === "Management") {
+                            return 5;
+                        }
+                    }
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(`${answer.roleTitle} role added!`);
+                    inquirer.prompt([
+                        {
+                            name: "another",
+                            type: "confirm",
+                            message: "Would you like to make another role?"
+                        }
+                    ]).then(function (answer) {
+                        if (answer.another === true) {
+                            addRole();
+                        } else {
+                            start();
+                        }
+                    });
+                }
+            );
+        });
+    });
+}
+
+function addEmployee() {
+    console.log("Please enter the new employee info...");
+    inquirer.prompt([
+        {
+            name: "employeeFirstName",
+            type: "input",
+            message: "What is the first name of the new employee?"
+        },
+        {
+            name: "employeeLastName",
+            type: "input",
+            message: "What is the last name of the new employee?"
+        },
+    ]).then(function (answer) {
+        connection.query(
+            "INSERT INTO employee SET ?",
+            {
+                first_name: answer.employeeFirstName,
+                last_name: answer.employeeLastName
+            },
+            function (err, res) {
+                if (err) throw err;
+                console.log(`${answer.employeeFirstName} ${answer.employeeLastName} has been added!`);
+                inquirer.prompt([
+                    {
+                        name: "another",
+                        type: "confirm",
+                        message: "Would you like to make another employee?"
+                    }
+                ]).then(function (answer) {
+                    if (answer.another === true) {
+                        addEmployee();
+                    } else {
+                        start();
+                    }
+                });
+            }
+        );
     });
 }
